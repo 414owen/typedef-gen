31abfbc914305949b44b575941afeffa45123aaf
Owen Shepherd <414owen@gmail.com>
2018-02-22 12:24:01 +0000

    Don't alias non-template types, allow line dumping

--- alias.cpp
+++ alias.cpp
@@ -11,20 +11,15 @@ ifstream in;
 ofstream out;
 
 int layers = 2;
+vector<string> dump;
 // [name, alias]
 vector<pair<string, string>> prims;
 // [name, arity, alias]
 vector<tuple<string, int, string>> types;
 
-void err(size_t line, string s) {
-	cerr << "Error on line: " << line << " " << s << endl;
-	exit(1);
-}
-
 vector<string> spl(string s, string delims) {
 	vector<string> res;
 	boost::split(res, s, boost::is_any_of(delims));
-	// remove_if(res.begin(), res.end(), empty_str);
 	res.erase(std::remove(res.begin(), res.end(), ""), res.end());
 	return res;
 }
@@ -44,14 +39,22 @@ void interpret() {
 				if (s == "->") break;
 				type += " " + s;
 			}
-			if (i != strs.size() - 2) err(line_n, "unrecognised primitive format");
+			if (i != strs.size() - 2) {
+				dump.push_back(line);
+				continue;
+			}
 			prims.push_back({type, strs.back()});
 		} else if (strs.size() == 2) {
 			// template
 			vector<string> strl = spl(strs[0], "<>");
-			if (strl.size() != 2) err(line_n, "Unrecognized template format");
+			if (strl.size() != 2) {
+				dump.push_back(line);
+				continue;
+			}
 			vector<string> strt = spl(strl[1], "-");
-			if (strl.size() > 2) err(line_n, "Unrecognized template range");
+			if (strl.size() > 2) {
+				dump.push_back(line);
+			}
 			int start = stoi(strt[0]), end;
 			if (strt.size() == 1) end = start;
 			else end = stoi(strt[1]);
@@ -59,35 +62,36 @@ void interpret() {
 				types.push_back({strl[0], i, strs[1]});
 			}
 		} else if (strs.size() != 0) {
-			err(line_n, "unrecognised amount of tokens");
+			dump.push_back(line);
 		}
 		line_n++;
 	}
 }
 
 pair<string, string> new_type(vector<int> inds, tuple<string, int, string> t,
-		vector<pair<string, string>> prev) {
+		vector<pair<string, string>> prev, bool full) {
 	string name = get<0>(t);
 	int arity = get<1>(t);
 	string alias = get<2>(t);
 	string newname = name + '<';
 	string newalias = alias;
 	for (int j = 0; j < arity - 1; j++) {
-		newname += get<1>(prev[inds[j]]) + ',';
+		newname += (full ? get<0>(prev[inds[j]]) : get<1>(prev[inds[j]])) + ',';
 		newalias += get<1>(prev[inds[j]]);
 	}
-	newname  += get<1>(prev[inds.back()]) + '>';
+	newname += (full ? get<0>(prev[inds.back()]) : get<1>(prev[inds.back()])) + '>';
 	newalias += get<1>(prev[inds.back()]);
 	return {newname, newalias};
 }
 
-vector<pair<string, string>> next_layer(vector<pair<string, string>> prev) {
+vector<pair<string, string>> next_layer(vector<pair<string, string>> prev,
+		bool full) {
 	vector<pair<string, string>> next;
 
 	for (auto el : types) {
 		vector<int> inds(get<1>(el));
 		while (true) {
-			next.push_back(new_type(inds, el, prev));
+			next.push_back(new_type(inds, el, prev, full));
 			bool overflow = true;
 			for (int j = 0; j < get<1>(el); j++) {
 				if (inds[j] + 1 < prev.size()) {
@@ -104,12 +108,13 @@ vector<pair<string, string>> next_layer(vector<pair<string, string>> prev) {
 
 void run() {
 	int layer = 0;
+	for (string s : dump) (outfile ? out : cout) << s << '\n';
 	vector<pair<string, string>> prev = prims;
 	for (int i = 0; i < layers; i++) {
-		prev = next_layer(prev);
+		prev = next_layer(prev, i == 0);
 		for (auto a : prev)
 			(outfile ? out : cout) << "typedef " <<
-				get<0>(a) << ' ' << get<1>(a) << ';' << endl;
+				get<0>(a) << ' ' << get<1>(a) << ';' << '\n';
 	}
 }
 
